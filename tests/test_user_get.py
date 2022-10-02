1b4f34c4300a97c5c3611b84299c6032e7088641
TonyKat <anton.kalaturskii@yandex.ru>
2022-10-03 00:52:51 +0300

    fix Ex16: Запрос данных другого пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -6,8 +6,6 @@ from lib.assertions import Assertions
 
 
 class TestUserGet(BaseCase):
-    # user_ids = [new_id for new_id in range(3, 5)]
-
     def test_get_user_not_details_not_auth(self):
         response = MyRequests.get('/user/2')
 
@@ -39,7 +37,6 @@ class TestUserGet(BaseCase):
         expected_field = ['username', 'email', 'firstName', 'lastName']
         Assertions.assert_json_has_keys(response2, expected_field)
 
-    # @pytest.mark.parametrize('user_id', user_ids)
     def test_get_user_details_auth_as_other_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
