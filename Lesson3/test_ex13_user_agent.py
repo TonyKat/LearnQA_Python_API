845b38efc39daf117e7663dd3c01aa726616317a
TonyKat <anton.kalaturskii@yandex.ru>
2022-10-02 18:50:13 +0300

    Ex13: fix name of class

--- Lesson3/test_ex13_user_agent.py
+++ Lesson3/test_ex13_user_agent.py
@@ -6,7 +6,7 @@ from Lesson3.lib.base_case import BaseCase
 from Lesson3.lib.assertions import Assertions
 
 
-class TestRequestToHeader(BaseCase):
+class TestUserAgent(BaseCase):
     exclude_params = [
         {
             'user_agent': 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
